ba00aede1640462ce15c809043a89353ebe1151b
Your Name <Berindeiee>
2023-11-28 12:03:21 +0200

    lab5

--- Paragraph.java
+++ Paragraph.java
@@ -1,12 +1,38 @@
-class Paragraph implements Element {
+public class Paragraph implements Element {
     private String text;
+
+    private AlignStrategy alignStrategy;
 
     public Paragraph(String text) {
         this.text = text;
+        alignStrategy = null;
     }
 
     @Override
     public void print() {
-        System.out.println("Paragraph: " + text);
+        if (alignStrategy != null)
+            alignStrategy.render(text);
+        else
+            System.out.println("Paragraph: " + text);
+    }
+
+    @Override
+    public void addContent(Element e) {
+        this.addContent(e);
+    }
+
+    @Override
+    public void remove(Element e) {
+        this.remove(e);
+    }
+
+    @Override
+    public Element get(int index) {
+        return this.get(index);
+    }
+
+
+    public void setAlignStrategy(AlignStrategy alignStrategy) {
+        this.alignStrategy = alignStrategy;
     }
 }
